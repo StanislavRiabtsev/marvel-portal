a058acb12cd6963c51f0bf50109af073ccc69d66
Stanislav Riabtsev <76626507+StanislavRiabtsev@users.noreply.github.com>
2025-09-04 15:19:26 +0200

    Third-party library errors and focus problem

--- src/components/charList/CharList.jsx
+++ src/components/charList/CharList.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef, useMemo } from 'react';
 import PropTypes from 'prop-types';
 
 import Spinner from '../spinner/Spinner';
@@ -28,10 +28,11 @@ const CharList = (props) => {
     const [offset, setOffset] = useState(1);
     const [charEnded, setCharEnded] = useState(false);
 
-    const { loading, error, process, getAllCharacters, setProcess } = useMarvelService();
+    const { process, getAllCharacters, setProcess } = useMarvelService();
 
     useEffect(() => {
         onRequest(offset, true);
+        // eslint-disable-next-line
     }, [])
 
     const onRequest = (offset, initial) => {
@@ -92,9 +93,14 @@ const CharList = (props) => {
         )
     }
 
+    const elements = useMemo(() => {
+        return setContent(process, () => renderItems(charList), newItemLoading);
+        // eslint-disable-next-line
+    }, [process]);
+
     return (
         <div className="char__list">
-            {setContent(process, () => renderItems(charList), newItemLoading)}
+            {elements}
             <button
                 className="button button__main button__long"
                 disabled={newItemLoading}
